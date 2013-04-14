bc02285159663425aa8e0a4f66bd8168747d5421
ljfranklin <lylejfranklin@gmail.com>
2013-04-14 12:19:04 -0400

    Added basic implementation for loading project data from json

--- Pages/Work/WorkView.js
+++ Pages/Work/WorkView.js
@@ -2,18 +2,38 @@
 define([
     'backbone',
     'underscore',
-    'text!./templates/work-template.html'
-], function(Backbone, _, workTemplate) {
+    'text!./templates/work-template.html',
+    './scripts/app/ProjectPreviewView',
+    'text!./data/projects.json'
+], function(Backbone, _, workTemplate, ProjectPreviewView, projectsJsonRaw) {
+
+    var projectsJson = JSON.parse(projectsJsonRaw);
 
     var WorkView = Backbone.View.extend({
         initialize: function() {
             this.render();
         },
         render: function() {
-            var template = _.template(workTemplate);
+
+            var template = _.template(workTemplate, {
+                projectPreviewViews: getProjectPreviewViews()
+            });
             this.$el.html(template);
         }
     });
+
+    function getProjectPreviewViews() {
+        var views = [];
+
+        _.each(projectsJson.projects, function(project) {
+            var projectPreviewView = new ProjectPreviewView({
+                projectJson: project
+            });
+            views.push(projectPreviewView.render());
+        });
+
+        return views;
+    }
 
     return WorkView;
 });
---
+++ Pages/Work/scripts/app/ProjectPreviewView.js
@@ -0,0 +1,28 @@
+
+define([
+    'backbone',
+    'underscore',
+    'text!../../templates/project-preview-template.html'
+], function(Backbone, _, projectPreviewTemplate) {
+
+    var ProjectPreviewView = Backbone.View.extend({
+        initialize: function() {
+
+        },
+        render: function() {
+
+            var projectData = this.options.projectJson;
+            var img = new Image();
+            img.onload(function() {
+                
+            });
+            img.src = projectData.previewImg;
+
+
+            var template = _.template(projectPreviewTemplate, projectData);
+            return template;
+        }
+    });
+
+    return ProjectPreviewView;
+});
