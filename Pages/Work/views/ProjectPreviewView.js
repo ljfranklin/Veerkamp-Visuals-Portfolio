e0871f56df8efc6970d911bdfcd829f4da6c7a3e
ljfranklin <lylejfranklin@gmail.com>
2013-05-26 17:00:49 -0400

    Added image to work page

--- Pages/Work/views/ProjectPreviewView.js
+++ Pages/Work/views/ProjectPreviewView.js
@@ -11,11 +11,17 @@ define([
             var self = this;
             var model = self.model;
 
+            var containerSelector = '.project-preview-container[data-project-cid="' + model.cid + '"]';
+
             self.render = function() {
 
                 var img = new Image();
                 img.onload = function() {
                     console.log("Loaded");
+
+                    var $container = $(containerSelector);
+                    var $imageContainer = $container.find('.preview-img-container');
+                    $imageContainer.append(img);
                 };
                 img.src = model.get('previewImg');
 
