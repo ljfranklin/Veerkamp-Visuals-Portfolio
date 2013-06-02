696b7c6e503cd22e89b2636f4a1ff0cf6da0995a
ljfranklin <lylejfranklin@gmail.com>
2013-06-02 17:51:33 -0400

    Added basic form to contact modal

--- common/scripts/app/contact.js
+++ common/scripts/app/contact.js
@@ -4,15 +4,28 @@ define([
 ], function() {
 
     var self = {};
+    var modalSelector = '.contact-modal';
 
     self.init = function() {
         $(document).on('click', '.contact-link', showModal);
+        $(document).on('click', '.contact-modal .submit', submitEmail);
     };
 
     function showModal() {
-        $('.contact-modal').modal('show');
+        $(modalSelector).modal('show');
+
+        $(modalSelector).find('#user-message').val('');
+    }
+
+    function submitEmail() {
+
+        var $modal = $(modalSelector);
+
+        var userEmail = $modal.find('#user-email').val();
+        var userMessage = $modal.find('#user-message').val();
+
+        console.log(userEmail + ' - ' + userMessage);
     }
 
     return self;
-
 });
